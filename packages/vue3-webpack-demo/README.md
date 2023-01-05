1ef8b65dbb2967b32f7b7e206da379707a20e21f
xinxu <xin872906473@163.com>
2023-01-05 15:48:00 +0800

    webpack vue3 debugger

---
+++ packages/vue3-webpack-demo/README.md
@@ -0,0 +1,31 @@
+# vue3-webpack-demo
+
+要正确 sourcemap 到源码，需要将源码下载到 packages 中
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ packages/vue3-webpack-demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div @click="aClick()">{{ a }}</div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const a = ref("666");
+    const aClick = (e) => {
+      console.log(e);
+      a.value = "999";
+    };
+    return {
+      a,
+      aClick,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
