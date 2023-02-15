dec502e65873dd896992af2fe7f93eff8c4a8937
xinxu <xin872906473@163.com>
2023-02-15 21:27:41 +0800

    debug vite config.ts

--- packages/vue-debug-demo2/vite.config.ts
+++ packages/vue-debug-demo2/vite.config.ts
@@ -4,12 +4,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+const plugins = [vue(), vueJsx()];
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
+const config = defineConfig({
+  plugins,
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
+
+export default config;
