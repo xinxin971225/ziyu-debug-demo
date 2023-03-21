c57b2afe6bc1c801f57d26a57745756875f9fd84
xinxu <xin872906473@163.com>
2023-03-21 10:26:04 +0800

    break point test

--- packages/test-react-debug/src/App.js
+++ packages/test-react-debug/src/App.js
@@ -5,7 +5,12 @@ import { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div
+        className="App"
+        onClick={(e) => {
+          console.log(e);
+        }}
+      >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
